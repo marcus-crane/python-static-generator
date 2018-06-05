5a476feb6c0b6a30072c9d7536e9ee4ae83562dd
Marcus Crane <marcus.crane@xero.com>
2018-06-05 15:15:42 +1200

    Added markdown render test

--- test/markdown/test_renderer.py
+++ test/markdown/test_renderer.py
@@ -1,5 +1,6 @@
 from markdown.renderer import PostRenderer
 
+import mistune
 
 def test_render_code_no_lang():
     code = "print('hello world')"
@@ -15,4 +16,23 @@ def test_renderer_code_with_lang():
     expected = f'<pre class="code" data-lang="python"><code>{code}</code></pre>'
     renderer = PostRenderer()
     actual = renderer.block_code(code, lang)
+    assert expected == actual
+
+
+def test_code_render_no_lang_markdown():
+    code = """```\nprint("hi")\n```"""
+    expected = f'<pre class="code"><code>print("hi")</code></pre>'
+    renderer = PostRenderer()
+    render = mistune.Markdown(renderer=renderer)
+    actual = render(code)
+    assert expected == actual
+
+
+def test_code_render_lang_markdown():
+    code = """```javascript\nconsole.log("hello")\n```"""
+    expected = f'<pre class="code" data-lang="javascript">' \
+               f'<code>console.log("hello")</code></pre>'
+    renderer = PostRenderer()
+    render = mistune.Markdown(renderer=renderer)
+    actual = render(code)
     assert expected == actual
